2ca478501c5427e6660b9cfa1362cb231742ead8
91964 <yeswanthsoma83@gmail.com>
2025-11-13 23:11:23 -0800

    Improve documentation

--- code-snippet-manager.py
+++ code-snippet-manager.py
@@ -235,6 +235,7 @@ class CodeSnippetManager(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = CodeSnippetManager()
+# Added comment
     window.show()
     sys.exit(app.exec())
 
